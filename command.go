6f67d28413e51422da42e260bd0e4a1957582c23
Viliam Dias <viliamjr@gmail.com>
2014-06-10 21:48:38 -0300

    Adicionando commando 'runtime' como builtin.

--- command.go
+++ command.go
@@ -2,6 +2,7 @@ package prompt
 
 import (
 	"fmt"
+	"runtime"
 )
 
 type Command struct {
@@ -14,9 +15,9 @@ func (c *Command) Run() bool {
 
 	if c.Cmd == "quit" {
 		return true
-	} else if c.Cmd == "" {
-		return false
 	}
+
+	builtinCommand(c)
 
 	if c.Action != nil {
 
@@ -43,4 +44,27 @@ func Parse(txt string) *Command {
 	}
 
 	return &Command{Cmd: txt, Action: nil}
+}
+
+func builtinCommand(c *Command) {
+	if c.Cmd == "" {
+		c.Action = func() {}
+	} else if c.Cmd == "runtime" || c.Cmd == "r" {
+		c.Action = func() {
+			var s runtime.MemStats
+			runtime.ReadMemStats(&s)
+
+			fmt.Println("###")
+			fmt.Println("# Logical CPUs:", runtime.NumCPU())
+			fmt.Println("# Goroutines  :", runtime.NumGoroutine())
+			fmt.Println("# Memory")
+			fmt.Println("#- Allocated (bytes)")
+			fmt.Println("# |- General:", s.Alloc)
+			fmt.Println("# |- Heap   :", s.HeapAlloc)
+			fmt.Println("#- Number of")
+			fmt.Println("# |- Mallocs:", s.Mallocs)
+			fmt.Println("# |- Frees  :", s.Frees)
+			fmt.Println("###")
+		}
+	}
 }
