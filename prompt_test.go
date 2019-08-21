acc45072a9f76dc9d71836d3233c0fac941b7a7b
Viliam Dias <viliamjr@gmail.com>
2019-08-21 09:28:15 -0300

    Test case for the 'Command.run'

--- prompt_test.go
+++ prompt_test.go
@@ -1,6 +1,7 @@
 package prompt
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -21,4 +22,28 @@ func TestStart(t *testing.T) {
 	if cmd.Cmd != gcCmdName {
 		t.Errorf("GC command not found but '%s'\n", commands[0].Cmd)
 	}
+}
+
+func TestRunCommand(t *testing.T) {
+	cmd := &Command{
+		Cmd:  "ping",
+		Desc: "just an example",
+		Action: func(args []string) {
+			fmt.Println("pong! Arguments:", args)
+		},
+	}
+
+	Add(*cmd)
+
+	Start()
+
+	// waiting for commands (gorountine started)
+	time.Sleep(time.Millisecond * 100)
+
+	cmdTarget := findCommand(cmd.Cmd)
+	if cmdTarget.Cmd != cmd.Cmd {
+		t.Errorf("%s command not found\n", cmd.Cmd)
+	}
+
+	cmdTarget.run([]string{})
 }
